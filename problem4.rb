05d5716161fdfb5dc99eafcfa2c8da5ee2e5f54e
Emmanuel Delgado <emmanuel.delgado@crowdint.com>
2011-01-08 11:07:35 -0600

    Finished problem # 4

---
+++ problem4.rb
@@ -0,0 +1,21 @@
+require 'set'
+
+def palindrome?(num)
+  num.to_s.reverse == num.to_s
+end
+
+def combinations_from(range1, range2)
+  nums = SortedSet.new
+  for counter1 in range1 do
+    for counter2 in range2 do
+      nums << counter1 * counter2 if palindrome?(counter1 * counter2)
+    end
+  end
+  nums
+end
+
+range = 2..999
+
+p combinations_from(range, range)
+
+
